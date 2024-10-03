2a0817fd13945f922bbe7e07d3ff0d43e09f8f54
islom-sattorov <slmsttrv1@gmail.com>
2024-10-03 15:30:09 +0500

    add: css config && yarn

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   plugins: [
     react(),
     tsconfigPaths(),
